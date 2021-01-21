7c5d2a91913326e2de2c171a8ad634ffc7eb9b83
junow <junow@rainist.com>
2021-01-22 00:52:55 +0900

    feat: 🎸 gegege

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,5 +2,6 @@ export const test = () => console.log('shared v0.5.1')
 export const test2 = () => console.log('im test from shared 222')
 console.log('betabeta')
 console.log('fix')
-console.log('test');
-console.log('test');
+console.log('test1');
+console.log('test2');
+console.log('test3');
