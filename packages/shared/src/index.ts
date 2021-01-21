beaa9f9ce8a61cdec053b4445054eb384ed7f741
junow <junow@rainist.com>
2021-01-22 00:15:20 +0900

    fix: 🐛 fixfix

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,4 @@ export const test = () => console.log('shared v0.5.1')
 export const test2 = () => console.log('im test from shared 222')
 console.log('betabeta')
 console.log('fix')
+console.log('test');
