1b3778348ac81f4fc2fc81ebf6b78a7eddd2070b
junow <junow@rainist.com>
2021-02-13 02:40:19 +0900

    fix: 🐛 add comment

--- packages/mono/src/index.ts
+++ packages/mono/src/index.ts
@@ -5,7 +5,7 @@ import {add} from '@junow/shared'
 // }d
 
 // export const sub2 = () => {
-//   return sub(1,2);dddddddddddddssssdd
+//   return sub(1,2);dddddddddddddssssdajdlTrnskchangesetd
 // }
 
 export const add2 = () => {
